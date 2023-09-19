6302fcd9080df32b74c8c03facf683b20b1ac8e6
alifaditya0 <danigilass@yahoo.com>
2023-09-19 15:37:02 +0700

    laporan3

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyPs = require("body-parser");
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
 const mhsRouter = require('./routes/mahasiswa');
+const { body } = require('express-validator');
 app.use('/api/mhs', mhsRouter);
 
 app.listen(port, () => {
