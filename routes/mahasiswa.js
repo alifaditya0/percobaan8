9a22eb05a3f38d433649516c607b4dd6f9c3ef7b
alifaditya0 <danigilass@yahoo.com>
2023-09-19 15:58:41 +0700

    laporan4

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -6,6 +6,20 @@ const {body, validationResult }= require('express-validator');
 const connection = require('../config/db');
 
 router.get('/', function (req, res){
+    connection.query('select * from mahasiswa order by id_m desc', function(err, rows){
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Failed',
+            })
+        }else{
+            return res.status(200).json({
+                status:true,
+                message: "Data Mahasiswa",
+                data: rows
+            })
+        }
+    })
 });
 
 router.post('/store', [
@@ -36,6 +50,33 @@ router.post('/store', [
             })
         }
     })
+})
+
+
+router.get('/(:id)', function (req, res) {
+let id=req.params.id;
+connection.query(`select * from mahasiswa where id_m = ${id}`, function (err, rows) {
+    if(err) {
+return res.status(500).json({
+status: false,
+message: 'Server Error',
+})
+}
+
+if(rows.length <=0){
+return res.status(404).json({
+status: false,
+message: 'Not Found',
+})
+}
+else{
+return res.status(200).json({ 
+status: true,
+message: 'Data Mahasiswa', 
+data: rows[0]
+})
+}
+})
 })
 
 module.exports = router;
