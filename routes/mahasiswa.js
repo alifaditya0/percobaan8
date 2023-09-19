d21cd48d3ee60e0f2f77f9e7ee487e6dc2090e00
alifaditya0 <danigilass@yahoo.com>
2023-09-19 16:22:00 +0700

    laporan6

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -110,4 +110,22 @@ message: 'Update Success..!'
 })
 })
 
+
+router.delete('/delete/(:id)', function(req, res){
+let id= req.params.id;
+connection.query(`delete from mahasiswa where id_m = ${id}`, function (err, rows) { 
+    if (err) {
+return res.status(500).json({
+status: false,
+message: 'Server Error',
+})
+}else{
+return res.status(200).json({
+status: true,
+message: 'Data has ben delete !',
+})
+}
+})
+})
+
 module.exports = router;
