13cc990c9ee003532e694575896efcac798f1cb3
alifaditya0 <danigilass@yahoo.com>
2023-10-10 14:43:31 +0700

    percobaan11

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,30 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const connection = require("../config/db");
 
-const {body, validationResult }= require('express-validator');
+const multer = require("multer")
+const path = require("path")
 
-const connection = require('../config/db');
+const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+        cb(null,'public/images')
+    },
+        filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, Date.now() + path.extname (file.originalname))
+    }
+})
+const upload = multer ({storage: storage});
 
-router.get('/', function (req, res){
-    connection.query('select  a.nama, b.nama_jurusan as jurusan ' + 'from mahasiswa a  join jurusan b  ' +  'on b.id_j=a.id_jurusan order by a.id_m  desc ', function(err, rows){
-        if(err){
-            return res.status(500).json({
-                status: false,
-                message: 'Server Failed',
-            })
-        }else{
-            return res.status(200).json({
-                status:true,
-                message: "Data Mahasiswa",
-                data: rows
-            })
-        }
-    })
-    });
+router.get("/", function (req, res) {
+  connection.query("SELECT a.nama, b.nama_jurusan AS jurusan FROM mahasiswa a JOIN jurusan b ON b.id_j = a.id_jurusan ORDER BY a.id_m DESC", function (err, rows) {
+    if (err) {
+      console.error(err); // Tampilkan error di konsol
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Data Mahasiswa",
+        data: rows,
+      });
+    }
+  });
+});
 
-router.post('/store', [
+router.post('/store', upload.single("gambar") , [
+    //validation
     body('nama').notEmpty(),
-    body('nrp').notEmpty()
+    body('nrp').notEmpty(),
+    body('id_jurusan').notEmpty()
 ],(req, res) => {
     const error = validationResult(req);
     if(!error.isEmpty()){
@@ -34,16 +49,19 @@ router.post('/store', [
     }
     let Data = {
         nama: req.body.nama,
-        nrp: req.body.nrp
+        nrp: req.body.nrp,
+        id_jurusan: req.body.id_jurusan,
+        gambar: req.file.filename
     }
     connection.query('insert into mahasiswa set ?', Data, function(err, rows){
         if(err){
             return res.status(500).json({
                 status: false,
                 message: 'Server Error',
+                error:  err
             })
         }else{
-            return  res.status(201).json({
+            return res.status(201).json({
                 status: true,
                 message: 'Success..!',
                 data: rows[0]
@@ -52,80 +70,72 @@ router.post('/store', [
     })
 })
 
-
-router.get('/(:id)', function (req, res) {
-let id=req.params.id;
-connection.query(`select * from mahasiswa where id_m = ${id}`, function (err, rows) {
-    if(err) {
-return res.status(500).json({
-status: false,
-message: 'Server Error',
-})
-}
-
-if(rows.length <=0){
-return res.status(404).json({
-status: false,
-message: 'Not Found',
-})
-}
-else{
-return res.status(200).json({ 
-status: true,
-message: 'Data Mahasiswa', 
-data: rows[0]
-})
-}
-})
-})
-
-
-router.patch('/update/:id', [ 
-body('nama').notEmpty(), 
-body('nrp').notEmpty()
-], (req, res) => {
-const error = validationResult(req); 
-if(!error.isEmpty()){
-return res.status(422).json({
-});
-}
-let id = req.params.id;
-let Data = {
-    nama: req.body.nama,
-    nrp: req.body.nrp
-}
-
-connection.query(`update mahasiswa set? where id_m = ${id}`, Data, function (err, rows) {
-if(err){
-return res.status(500).json({
-    status: false,
-    message: 'Server Error',
-})
-} else {
-return res.status(200).json({ 
-    status: true,
-message: 'Update Success..!'
-})
-}
-})
-})
-
-
-router.delete('/delete/(:id)', function(req, res){
-let id= req.params.id;
-connection.query(`delete from mahasiswa where id_m = ${id}`, function (err, rows) { 
+router.get("/(:id)", function (req, res) {
+  let id = req.params.id;
+  connection.query(`SELECT * From mahasiswa where id_m = ${id}`, function (err, rows) {
     if (err) {
-return res.status(500).json({
-status: false,
-message: 'Server Error',
-})
-}else{
-return res.status(200).json({
-status: true,
-message: 'Data has ben delete !',
-})
-}
-})
-})
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    }
+    if (rows.length <= 0) {
+      return res.status(404).json({
+        status: false,
+        message: "Not Found",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Data Mahasiswa",
+        data: rows[0],
+      });
+    }
+  });
+});
+
+router.patch("/update/(:id)", [body("nama").notEmpty(), body("nrp").notEmpty()], (req, res) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      error: error.array(),
+    });
+  }
+  let id = req.params.id;
+  let Data = {
+    nama: req.body.nama,
+    nrp: req.body.nrp,
+  };
+  connection.query(`UPDATE mahasiswa set ? where id_m = ${id}`, Data, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Update Sukses..!",
+      });
+    }
+  });
+});
+
+router.delete("/delete/(:id)", function (req, res) {
+  let id = req.params.id;
+  connection.query(`DELETE From mahasiswa where id_m = ${id}`, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Data has ben delete..!!",
+      });
+    }
+  });
+});
 
 module.exports = router;
