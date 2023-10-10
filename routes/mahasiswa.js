f15acb6a920783b6d836428bba816dc677341546
alifaditya0 <danigilass@yahoo.com>
2023-10-10 15:19:46 +0700

    percobaan14

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,194 +1,214 @@
-const express = require('express')
-const router = express.Router()
-
-const connection = require("../config/db")
-const {body, validationResult} = require('express-validator')
-
-const multer = require('multer')
-const path = require('path')
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const connection = require("../config/db");
 const fs = require('fs')
+const multer = require("multer")
+const path = require("path")
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null,'public/images')
-    }, filename: (req,file,cb) => {
-        console.log(file)
-        cb(null, Date.now() + path.extname(file.originalname))
+
+  const filefilter = (reg,file, cb) => {
+    // Mengecek jenis file yang diizinkan (misalnya, hanya gambar JPEG atau PNG)
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true); // Izinkan file
+    } else {
+      cb(new Error('Jenis file tidak diizinkan'), false); // Tolak file
     }
-})
+  };
 
-const upload = multer({storage: storage})
-
-router.get('/', function(req,res){
-    connection.query(' SELECT mahasiswa.nama, jurusan.nama_jurusan '+' from mahasiswa join jurusan '+' ON mahasiswa.id_jurusan=jurusan.id_j order by mahasiswa.id_m desc', function(err, rows){
-        if(err){
-            return res.status(500).json({
-                status: false,
-                message: 'server failed',
-                error: err
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message:'Data mahasiswa',
-                data: rows
-            })
-        }
+  const storage = multer. diskStorage({
+    destination: (reg, file, cb) => {
+    cb(null,'public/images')
+   },
+    filename: (reg, file, cb) => {
+    console.log(file)
+    cb(null, Date.now() + path.extname (file.originalname) )}
     })
-})
+    const upload = multer ({storage: storage,fileFilter:filefilter});
+  
 
-router.post('/store',upload.single("gambar"), [
-    body('nama').notEmpty(),
-    body('nrp').notEmpty(),
-    body('id_jurusan').notEmpty(),
-], (req, res) => {
-    const error = validationResult(req)
-    if(!error.isEmpty()){
-        return res.status(422).json({
-            error: error.array()
-        })
+router.get("/", function (req, res) {
+  connection.query(
+    
+    "SELECT a.nama, b.nama_jurusan AS jurusan FROM mahasiswa a JOIN jurusan b ON b.id_j = a.id_jurusan ORDER BY a.id_m DESC",
+   function (err, rows) {
+    if (err) {
+      console.error(err); // Tampilkan error di konsol
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Data Mahasiswa",
+        data: rows,
+      });
+    }
+  });
+});
+
+router.post(
+  "/store",upload.fields([{ name: 'gambar', maxCount: 1 }, { name: 'swa_foto', maxCount: 1 }]),
+  [
+    body("nama").notEmpty(),
+    body("nrp").notEmpty(),
+    body("id_jurusan").notEmpty(), // Menambah validasi untuk jurusan
+  ],
+  (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(422).json({
+        error: error.array(),
+      });
     }
     let Data = {
-        nama: req.body.nama,
-        nrp:req.body.nrp,
-        id_jurusan:req.body.id_jurusan,
-        gambar: req.file.filename
+      nama: req.body.nama,
+      nrp: req.body.nrp,
+      id_jurusan: req.body.id_jurusan, 
+      gambar: req.files.gambar[0].filename, 
+      swa_foto: req.files.swa_foto[0].filename
+      
+    };
+    connection.query("INSERT into mahasiswa set ? ", Data, function (err, rows) {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Server Error",
+        });
+      } else {
+        return res.status(201).json({
+          status: true,
+          message: "Sukses..!",
+          data: rows[0],
+        });
+      }
+    });
+  }
+);
+
+router.get("/(:id)", function (req, res) {
+  let id = req.params.id;
+  connection.query(`SELECT * From mahasiswa where id_m = ${id}`, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
     }
-    connection.query('insert into mahasiswa set ? ', Data, function(err, rows){
-        if(err){
-            return res.status(500).json({
-                status: false,
-                message: 'server failed'
-            })
-        } else {
-            return res.status(201).json({
-                status: true,
-                message:'Success',
-                data: rows[0]
-            })
-        }
-    })
-})
-
-router.get('/(:id)', function(req,res) {
-    let id= req.params.id
-    connection.query(`select * from mahasiswa where id_m = ${id}`, function(err,rows){
-        if(err){
-            return res.status(500).json({
-                status: false,
-                message: 'server error'
-            })
-        }
-        if(rows.length <=0){
-            return res.status(404).json({
-                status: false,
-                message: 'Not Found'
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message:'data mahasiswa',
-                data: rows[0]
-            })
-        }
-    })
-})
-
-router.patch('/update/:id',upload.single("gambar"),[
-    body('nama').notEmpty(),
-    body('nrp').notEmpty(),
-    body('id_jurusan').notEmpty(),
-], (req,res) => {
-    const error = validationResult(req)
-    if(!error.isEmpty()){
-        return res.status(422).json({
-            error: error.array()
-        })
+    if (rows.length <= 0) {
+      return res.status(404).json({
+        status: false,
+        message: "Not Found",
+      });
+    }else {
+      return res.status(200).json({
+        status: true,
+        message: "Data Mahasiswa",
+        data: rows[0],
+      });
     }
-    let id = req.params.id
+  });
+});
 
-    let gambar = req.file ? req.file.filename : null;
-    
-    
-    connection.query(`select * from mahasiswa where id_m = ${id}`, function(err,rows){
-        if(err){
-            return res.status(500).json({
-                status: false,
-                message: 'server error'
-            })
-        }
-        if(rows.length === 0){
-            return res.status(404).json({
-                status: false,
-                message: 'Not Found'
-            })
-        }
-        const namaFileLama = rows[0].gambar
-        
-        if(namaFileLama && gambar){
-            const pathFileLama = path.join(__dirname, '../public/images', namaFileLama)
-            fs.unlinkSync(pathFileLama)
-        }
-        
-        let data = {
-            nama:req.body.nama,
-            nrp:req.body.nrp,
-            id_jurusan:req.body.id_jurusan,
-            gambar: gambar,
-        }
 
-        connection.query(`update mahasiswa set ? where id_m = ${id}`, data, function(err,rows){
-            if(err){
-                return res.status(500).json({
-                    status: false,
-                    message: 'server error'
-                })
-            } else {
-                return res.status(200).json({
-                    status: true,
-                    message: 'update'
-                })
-            }
-        })
-    })
-})
+router.patch("/update/:id", upload.single("gambar"), [
+  body("nama").notEmpty(),
+  body("nrp").notEmpty(),
+  body("id_jurusan").notEmpty(),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errors: errors.array(),
+    });
+  }
 
-router.delete('/delete/(:id)', function(req, res){
-    let id = req.params.id
-    connection.query(`select * from mahasiswa where id_m = ${id}`, function(err,rows){
-        if(err){
-            return res.status(500).json({
-                status: false,
-                message: 'server error'
-            })
-        }
-        if(rows.length <=0){
-            return res.status(404).json({
-                status: false,
-                message: 'Not Found'
-            })
-        }
-        const namaFileLama = rows[0].gambar
-        
-        if(namaFileLama){
-            const pathFileLama = path.join(__dirname, '../public/images', namaFileLama)
-            fs.unlinkSync(pathFileLama)
-        }
-        
-        connection.query(`delete from mahasiswa where id_m = ${id}`, function(err, rows){
-            if(err){
-                return res.status(500).json({
-                    status: false,
-                    message: 'server error'
-                })
-            } else {
-                return res.status(200).json({
-                    status: true,
-                    message: 'Data di hapus'
-                })
-            }
-        })
-    })
-})
+  const id = req.params.id;
+  const gambar = req.file ? req.file.filename : null;
 
-module.exports = router
+  connection.query(`SELECT * FROM mahasiswa WHERE id_m = ${id}`, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    }
+    if (rows.length === 0) {
+      return res.status(404).json({
+        status: false,
+        message: "Not Found",
+      });
+    }
+
+    const namaFileLama = rows[0].gambar;
+
+    if (namaFileLama && gambar) {
+      const pathFileLama = path.join(__dirname, '../public/images', namaFileLama);
+      fs.unlinkSync(pathFileLama);
+    }
+
+    let Data = {
+      nama: req.body.nama,
+      nrp: req.body.nrp,
+      id_jurusan:req.body.id_jurusan,
+      gambar: gambar
+    };
+
+    connection.query(`UPDATE mahasiswa SET ? WHERE id_m = ${id}`, Data, function (err, result) {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Update Sukses..!",
+        });
+      }
+    });
+  });
+});
+
+router.delete("/delete/(:id)", function (req, res) {
+  const id = req.params.id;
+
+  connection.query(`SELECT * FROM mahasiswa WHERE id_m = ${id}`, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Server Error",
+      });
+    }
+    if (rows.length === 0) {
+      return res.status(404).json({
+        status: false,
+        message: "Not Found",
+      });
+    }
+
+    const namaFileLama = rows[0].gambar;
+
+    if (namaFileLama) {
+      const pathFileLama = path.join(__dirname, '../public/images', namaFileLama);
+      fs.unlinkSync(pathFileLama);
+    }
+
+    connection.query(`delete from mahasiswa where id_m = ${id}`, function (err, result) {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Deleted Sukses..!",
+        });
+      }
+    });
+  });
+});
+
+module.exports = router;
